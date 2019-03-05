40adadd3d536ca3b652a98eb83ab54c99f275d3a
Patryk Stanek <p.stanek3@gmail.com>
2019-03-05 16:56:22 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 'use strict';
 
-function Phone(brand, price, color) {
+function Phone(name, brand, price, color, year) {
+    this.name = name;
     this.brand = brand;
     this.price = price;
     this.color = color;
+    this.year = year;
 };
 
 Phone.prototype.printInfo = function () {
-    console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+    console.log(this.name + ' - the phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '. Release date: ' + this.year + '.');
 }
 
-var iphone = new Phone('Apple', 2250, 'silver');
-var samsung = new Phone('Samsung', 2240, 'black');
-var oneplus = new Phone('Oppo', 2230, 'white');
+var iphone = new Phone('iPhone 6s', 'Apple', 2250, 'silver', 2015);
+var samsung = new Phone('Samsung Galaxy S7', 'Samsung', 2240, 'black', 2017);
+var oneplus = new Phone('OnePlus One', 'Oppo', 2230, 'white', 2008);
 
 iphone.printInfo();
 samsung.printInfo();
